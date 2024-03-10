cef97b59ce7b331a748130f662aecf7702409896
chaybonnada <119953351+chaybonnada@users.noreply.github.com>
2024-03-10 14:40:36 +0530

    Feature (#8)
    * Sample HTML file
    
    * Git commit check
    
    * Git commit check
    
    * Create Repoclone.sh
    
    * Update Repoclone.sh
    
    ---------
    
    Co-authored-by: Chaitanya <chaitanyabonnada@gmail.com>

--- CICD_Project/Repoclone.sh
+++ CICD_Project/Repoclone.sh
@@ -7,15 +7,28 @@ REPO_URL="https://github.com/chaybonnada/Hero_CICD_Project.git"
 DESTINATION="/user/chaitu/chaitu"
 
 # Check if Nginx is installed
-if ! command -v nginx &> /dev/null
-then
+if ! command -v nginx &> /dev/null; then
     echo "Nginx is not installed."
-else
-    echo "Nginx is already installed."
-    
-    # Clone the latest code from the GitHub repository
-    git clone "$REPO_URL" "$DESTINATION"
-    
-    # Restart Nginx
-    sudo systemctl restart nginx
+    exit 1
 fi
+
+# Clone the latest code from the GitHub repository
+git clone "$REPO_URL" "$DESTINATION"
+
+# Check if clone was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to clone code."
+    exit 1
+fi
+
+# Restart Nginx
+sudo systemctl restart nginx
+
+# Check if Nginx restart was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to restart Nginx."
+    exit 1
+fi
+
+echo "Code cloned and Nginx restarted successfully."
+exit 0
